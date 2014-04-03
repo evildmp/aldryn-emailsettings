bfc409cdd5f4cc5e6c3df7c4d9a101fb40ea498e
Stefan Foulis <stefan@foulis.ch>
2014-04-03 18:36:54 +0200

    fixes some errors and adds DEFAULT_FROM_EMAIL

--- aldryn_config.py
+++ aldryn_config.py
@@ -1,13 +1,16 @@
+# -*- coding: utf-8 -*-
 from aldryn_client.forms import BaseForm, CharField, NumberField, CheckboxField
 
 
 class Form(BaseForm):
 
-    email_host = CharField(default='localhost')
-    email_host_password = CharField(default='')
-    email_host_user = CharField(default='')
-    email_port = NumberField(default=25)
-    email_use_tls = CheckboxField()
+    email_host = CharField('SMTP Host', initial='localhost')
+    email_port = NumberField('SMTP Port', initial=25)
+    email_host_user = CharField('SMTP User', initial='')
+    email_host_password = CharField('SMTP Password', initial='')
+    email_use_tls = CheckboxField('Use TLS', required=False, initial=False)
+
+    default_from_email = CharField('Default from email', initial='', required=False)
 
     def to_settings(self, data, settings):
         email_settings = {
@@ -17,5 +20,8 @@ class Form(BaseForm):
             'EMAIL_PORT': data.get('email_port'),
             'EMAIL_USE_TLS': data.get('email_use_tls'),
         }
+        if data.get('default_from_email', False):
+            email_settings['DEFAULT_FROM_EMAIL'] = data.get('default_from_email')
+            email_settings['SERVER_EMAIL'] = data.get('default_from_email')
         settings.update(email_settings)
         return settings
