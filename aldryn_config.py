f52383aa05cc15f65a5e7061ef4ba18525b07996
Michał Ociepka <michal@ociepka.info>
2014-05-15 11:56:59 +0200

    Add opportunity to set develop email settings

--- aldryn_config.py
+++ aldryn_config.py
@@ -12,14 +12,30 @@ class Form(BaseForm):
 
     default_from_email = CharField('Default from email', initial='', required=False)
 
+    email_host_stage = CharField('SMTP Host used on stage', initial='localhost', required=False)
+    email_port_stage = NumberField('SMTP Port used on stage', initial=25, required=False)
+    email_host_user_stage = CharField('SMTP User used on stage', initial='', required=False)
+    email_host_password_stage = CharField('SMTP Password used on stage', initial='', required=False)
+    email_use_tls_stage = CheckboxField('Use TLS used on stage', required=False, initial=False)
+
     def to_settings(self, data, settings):
-        email_settings = {
-            'EMAIL_HOST': data.get('email_host'),
-            'EMAIL_HOST_PASSWORD': data.get('email_host_password'),
-            'EMAIL_HOST_USER': data.get('email_host_user'),
-            'EMAIL_PORT': data.get('email_port'),
-            'EMAIL_USE_TLS': data.get('email_use_tls'),
-        }
+        if settings.DEBUG:
+            email_settings = {
+                'EMAIL_HOST': data.get('email_host_stage') or data.get('email_host'),
+                'EMAIL_HOST_PASSWORD': data.get('email_host_password_stage') or data.get('email_host_password'),
+                'EMAIL_HOST_USER': data.get('email_host_user_stage') or data.get('email_host_user'),
+                'EMAIL_PORT': data.get('email_port_stage') or data.get('email_port'),
+                'EMAIL_USE_TLS': data.get('email_use_tls_stage') or data.get('email_use_tls'),
+            }
+        else:
+            email_settings = {
+                'EMAIL_HOST': data.get('email_host'),
+                'EMAIL_HOST_PASSWORD': data.get('email_host_password'),
+                'EMAIL_HOST_USER': data.get('email_host_user'),
+                'EMAIL_PORT': data.get('email_port'),
+                'EMAIL_USE_TLS': data.get('email_use_tls'),
+            }
+
         if data.get('default_from_email', False):
             email_settings['DEFAULT_FROM_EMAIL'] = data.get('default_from_email')
             email_settings['SERVER_EMAIL'] = data.get('default_from_email')
